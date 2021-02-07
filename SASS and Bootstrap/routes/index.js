89d5fa7794313572405acae22171d52507b82878
Samir Shah <samirshah120@gmail.com>
2021-02-07 14:52:12 -0500

    Delete SASS and Bootstrap directory

--- SASS and Bootstrap/routes/index.js
+++
@@ -1,9 +0,0 @@
-const layout = require('./layout');
-const constructorMethod = app => {
-    app.use("/", layout);
-
-    app.use("*", (req, res) => {
-        res.status(404).json({error:'Not found'});
-      });
-};
-module.exports = constructorMethod;
